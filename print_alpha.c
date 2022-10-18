5067465068ec9fa4a627237988df01f2ce53f703
fakile_john <fakilejohn@gmail.com>
2022-10-18 09:56:57 -0700

    betty fix

--- print_alpha.c
+++ print_alpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_string - loops through a string and prints
- * every character
+ * every character provided 
  * @l: va_list arguments from _printf
  * @f: pointer to the struct flags that determines
  * if a flag is passed to _printf
