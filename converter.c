5f093de4cfa0b2113655f92f0508ba86944d4962
fakile_john <fakilejohn@gmail.com>
2022-10-18 09:36:12 -0700

    updated converter.c

--- converter.c
+++ converter.c
@@ -3,7 +3,7 @@
  * convert - converts number and base into string
  * @num: input number
  * @base: input base
- * @lowercase: flag if hexa values need to be lowercase
+ * @lowercase: flag if hexadecimal values need to be lowercase
  * Return: result string
  */
 char *convert(unsigned long int num, int base, int lowercase)
