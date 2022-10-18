6a72eb0c1acc7c8726cc1dc6c255d51559b07dda
fakile_john <fakilejohn@gmail.com>
2022-10-18 10:08:26 -0700

    updated print_bases

--- print_bases.c
+++ print_bases.c
@@ -25,8 +25,7 @@ int print_hex(va_list l, flags_t *f)
  * print_hex_big - prints a number in hexadecimal base,
  * in uppercase
  * @l: va_list arguments from _printf
- * @f: pointer to the struct that determines
- * if a flag is passed to _printf
+ * @f: pointer to the struct that determines if a flag is passed to _printf
  * Description: the function calls convert() which in turns converts the input
  * number into the correct base and returns it as a string
  * Return: the number of char printed
