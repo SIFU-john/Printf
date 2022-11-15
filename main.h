a551461a7fd55858b3222efd2ac57f086707c401
Adegoke Ayobami <adegokeayobamiseun@gmail.com>
2022-11-15 07:18:15 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdlib.h>
 #include <stdarg.h>
 
@@ -41,7 +40,6 @@ int print_hex(va_list l, flags_t *f);
 int print_hex_big(va_list l, flags_t *f);
 int print_binary(va_list l, flags_t *f);
 int print_octal(va_list l, flags_t *f);
-
 /* converter */
 char *convert(unsigned long int num, int base, int lowercase);
 
